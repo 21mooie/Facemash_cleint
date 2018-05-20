df3e7d72f814b2b764ac662c4c23f84f3f01c65d
Muata Nkosi <mnkosi@fandm.edu>
2018-05-20 13:52:31 -0400

    6.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,17 @@ import { ApiService } from './api.service';
 import { HttpModule } from '@angular/http';
 import { MessagesComponent } from './messages.component'
 import { RegisterComponent } from './register.component';
+import { LoginComponent } from './login.component';
 
 const routes = [
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent}
 ]
 
 @NgModule({
   declarations: [
-    AppComponent, MessagesComponent , RegisterComponent
+    AppComponent, MessagesComponent , RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule, HttpModule, MatButtonModule, MatCardModule, 

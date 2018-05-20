f3afc5edaf032502e0ab76995732011a8c517317
Muata Nkosi <mnkosi@fandm.edu>
2018-05-20 19:49:48 -0400

    8.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { Component } from '@angular/core';
   template: `
       <mat-toolbar>
         Facemash
+        <button mat-button routerLink="/users">Users</button>
         <span style="flex: 1 1 auto"></span>
         <button mat-button routerLink="/register">Register</button>
       </mat-toolbar>
       <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.css']
+  `
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my app';

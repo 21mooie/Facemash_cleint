95fbd82fb57fea1be574a2bcd5129423c08403a6
Muata Nkosi <mnkosi@fandm.edu>
2018-05-20 12:18:02 -0400

    module 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <mat-toolbar>
+        Facemash
+      </mat-toolbar>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my app';
 }
